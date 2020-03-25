4f32840a2853454ba01ccb61fb0806d008fe63f8
Serhii Kononov <sergei97k@gmail.com>
2020-03-25 23:18:45 +0200

    installed svelte-loader for CharactersList

--- src/components/CharactersList.svelte
+++ src/components/CharactersList.svelte
@@ -1,20 +1,35 @@
 <div>
     <Filter />
 
-    {#each characters as character}
-        <div class="card">
-            <a rel="prefetch" href="characters/{character.char_id}">{character.name}</a>
-        </div>
-        {:else}
-        <p>No results</p>
-    {/each}
+    {#if $preloading}
+        <Spinner
+            size="50"
+            speed="750"
+            color="#A82124"
+            thickness="2"
+            gap="40"
+        />
+    {:else}
+        {#each characters as character}
+            <div class="card">
+                <a rel="prefetch" href="characters/{character.char_id}">{character.name}</a>
+            </div>
+            {:else}
+            <p>No results</p>
+        {/each}
+    {/if}
 
     <Pagination />
 </div>
 
 <script>
+    import { stores } from '@sapper/app';
+    import Spinner from 'svelte-spinner';
+
     import Pagination from './Pagination.svelte';
     import Filter from './Filter.svelte';
+
+    const { preloading } = stores();
 
     export let characters;
 </script>
