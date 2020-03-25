2f6719e9bde37ba834b3d2437c24f19570e0fccc
Serhii Kononov <sergei97k@gmail.com>
2020-03-25 22:13:40 +0200

    updated /characters request and passing params to api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,10 +1,13 @@
-const API_URL = 'https://www.breakingbadapi.com/api';
+import { stringify } from 'query-string';
 
-const api = (path) => {
+const API_URL = 'https://www.breakingbadapi.com/api';
+export const TOTAL_AMOUNT = 63;
+
+const api = (path, params) => {
   const fetch = process.browser ? window.fetch : require('node-fetch').default;
   const opts = { method: 'GET', headers: {} };
 
-  return fetch(`${API_URL}/${path}`, opts)
+  return fetch(`${API_URL}/${path}?${stringify(params)}`, opts)
     .then(r => r.text())
     .then(json => {
       try {
--- src/routes/characters/index.json.js
+++ src/routes/characters/index.json.js
@@ -1,7 +1,15 @@
 import api from '../../helpers/api';
 
+const DEFAULT_QUERY = {
+  offset: 0,
+  limit: 10
+};
+
 export function get(req, res) {
-  api('characters', req.query).then( data => {
+  // Need to set the default limit and offset params to API
+  const query = Object.keys(req.query).length ? req.query : DEFAULT_QUERY;
+
+  api('characters', query).then( data => {
     res.writeHead(200, {
       'Content-Type': 'application/json'
     });
